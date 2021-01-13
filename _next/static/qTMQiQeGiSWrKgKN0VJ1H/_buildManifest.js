9f8f82fd9c048adb437e5ac33b076768f5c9fe2f
web3012 <web3012@users.noreply.github.com>
2021-01-13 08:34:10 +0000

    deploy: web3012/test-gh-pages@0bb0bbce25bc8e5fcd18293e88364d7c93b0b5ca

---
+++ _next/static/qTMQiQeGiSWrKgKN0VJ1H/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-0043560742e83b1355ae.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e2ffa4674eea62b50f3f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
